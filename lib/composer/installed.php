84ab03daaa58c26a0a14ae7ecddf4d734d7440b3
Matthew Knighton <mrkwp@Matthews-MacBook-Pro.local>
2026-07-28 14:51:52 +1000

    Added in the Composer Auto loader.
    Updated NPM WP Scripts for latest version.

---
+++ lib/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mrkwp/rm-podacast',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '55f1fea8b05a4e7a26de688e52a32e477a28e15a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mrkwp/rm-podacast' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '55f1fea8b05a4e7a26de688e52a32e477a28e15a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
